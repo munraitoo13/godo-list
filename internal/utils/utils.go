cdcf8c5ebc7b7051aab5159af5252dfd790c1219
munraitoo13 <58868041+munraitoo13@users.noreply.github.com>
2024-12-30 11:01:40 -0300

    gambiarra for clearing term to work

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,22 +1,8 @@
 package utils
 
-import (
-	"os"
-	"os/exec"
-	"runtime"
-)
-
 // clear screen
 func ClearScreen() {
-	// switch based on the OS
-	switch runtime.GOOS {
-	case "windows":
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	default:
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
+	for i := 0; i < 100; i++ {
+		println()
 	}
 }
